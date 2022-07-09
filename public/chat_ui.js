bf79f226190d875d9f9fb62d171beacd67b5acef
zhaocongcong <zhaocongcong01@baijia.com>
2022-07-09 16:49:52 +0800

    add:做一些样式

--- public/chat_ui.js
+++ public/chat_ui.js
@@ -1,9 +1,23 @@
-const { createApp } = Vue;
+const { createApp, ref } = Vue;
 
 createApp({
   data() {
     return {
       message: "Hello Vue!",
+    };
+  },
+  setup(props) {
+    const text = ref("");
+    const textRef = ref();
+    const handlerSend = (e) => {
+      console.log(text.value);
+      text.value = "";
+      e.srcElement.blur();
+    };
+    return {
+      handlerSend,
+      text,
+      textRef,
     };
   },
 }).mount("#app");
